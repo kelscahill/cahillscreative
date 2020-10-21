5fa235f00dcaf129f79759201d56936014574897
Kelsey Cahill <kelscahill@gmail.com>
2020-10-21 12:17:03 -0400

    Ads plugin

---
+++ wp-content/plugins/advanced-ads/admin/views/settings/general/disable-shortcode-button.php
@@ -0,0 +1,2 @@
+<input id="advanced-ads-disable-shortcode-button" type="checkbox" <?php checked( $checked, true ); ?> name="<?php echo esc_attr( ADVADS_SLUG ); ?>[disable-shortcode-button]" />
+<p class="description"><?php esc_html_e( 'Disable shortcode button in visual editor.', 'advanced-ads' ); ?></p>
