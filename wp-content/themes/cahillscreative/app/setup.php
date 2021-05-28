22990af3c188d513d3e1caa34611d322201d97dd
Kelsey Cahill <kcahill@southleft.com>
2021-05-27 21:37:43 -0400

    Removed magnific popup.

--- wp-content/themes/cahillscreative/app/setup.php
+++ wp-content/themes/cahillscreative/app/setup.php
@@ -13,10 +13,6 @@ use Roots\Sage\Template\BladeProvider;
 add_action('wp_enqueue_scripts', function () {
     wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, null);
     wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], null, true);
-
-    if (is_single() || is_page()) {
-        wp_enqueue_script('sage/magnific-popup.js', asset_path('scripts/magnific-popup.js'), ['jquery'], null, true);
-    }
 }, 100);
 
 /**
--- wp-content/themes/cahillscreative/resources/assets/scripts/routes/common.js
+++ wp-content/themes/cahillscreative/resources/assets/scripts/routes/common.js
@@ -170,22 +170,6 @@ export default {
         }
       ]
     });
-
-    /**
-    * Magnigic Popup
-    */
-    if ($('.js-gallery').length) {
-      $('.js-gallery').each(function() {
-       $(this).magnificPopup({
-         delegate: 'a',
-         type: 'image',
-         gallery: {
-           enabled: true
-         }
-       });
-      });
-    }
-
     /**
      * Main class toggling function
      */
