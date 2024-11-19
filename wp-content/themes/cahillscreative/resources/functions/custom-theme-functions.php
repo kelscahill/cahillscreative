b310125c6aad67082dc5976632ad002fda0940f5
Kelsey Cahill <kelscahill@gmail.com>
2024-11-19 08:47:06 -0500

    Test for redirect

--- wp-content/themes/cahillscreative/resources/functions/custom-theme-functions.php
+++ wp-content/themes/cahillscreative/resources/functions/custom-theme-functions.php
@@ -7,10 +7,12 @@
  * @package WordPress
  */
 
-add_action('template_redirect', function () {
-  if (isset($_GET['_s'])) {
+ add_action('template_redirect', function () {
+  if (isset($_GET['_s']) && strpos($_GET['_s'], '-') !== false) {
+      // Replace hyphens with spaces
       $search = str_replace('-', ' ', $_GET['_s']);
-      wp_redirect('/shop/?_s=' . urlencode($search), 301);
+      // Redirect to the URL with spaces encoded as %20
+      wp_redirect('/shop/?_s=' . rawurlencode($search), 301);
       exit;
   }
 });
